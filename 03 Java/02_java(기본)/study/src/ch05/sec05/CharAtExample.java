67789754ee883f5ea6a09c6f81cda8041c091179
Hyukman818 <dhgurtls@gmail.com>
2025-04-17 16:51:20 +0900

    Feat : ch05.sec04 파일까지 추가

---
+++ 03 Java/02_java(기본)/study/src/ch05/sec05/CharAtExample.java
@@ -0,0 +1,10 @@
+package ch05.sec05;
+
+public class CharAtExample {
+    public static void main(String[] args) {
+        String ssn = "9506241230123";
+
+        char gender = ssn.charAt(6);
+        
+    }
+}
