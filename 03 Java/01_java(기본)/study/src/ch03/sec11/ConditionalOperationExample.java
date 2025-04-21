8a9041f19cad7c07b480007f0f458ee01faa007a
Hyukman818 <dhgurtls@gmail.com>
2025-04-21 17:13:21 +0900

    Feat : 01_java(기본) 실습 문제 완료

--- 03 Java/01_java(기본)/study/src/ch03/sec11/ConditionalOperationExample.java
+++ 03 Java/01_java(기본)/study/src/ch03/sec11/ConditionalOperationExample.java
@@ -5,6 +5,6 @@ public class ConditionalOperationExample {
         int score = 85;
         // 90 초과: A, 80 초과: B, 나머지: C
         char grade = (score > 90) ? 'A' : ( (score > 80) ? 'B' : 'C' );
-        System.out.println(score + "점은 " + grade + "등급입니다.");
+        System.out.println(score + "점은 " + grade + "등급입니다."); // printf로도 작성 가능
     }
 }
