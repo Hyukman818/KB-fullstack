2dc1101d01eda795226b9cde0534dbfadca197e6
Hyukman818 <dhgurtls@gmail.com>
2025-04-27 17:39:38 +0900

    Feat : 04_java(기본) 실습 문제 완료

--- 03 Java/04_java(기본)/study/src/ch07/sec04/exam01/Computer.java
+++ 03 Java/04_java(기본)/study/src/ch07/sec04/exam01/Computer.java
@@ -4,6 +4,7 @@ public class Computer extends Calculator{
     @Override
     public double areaCircle(double r) {
         System.out.println("Computer 객체의 areaCircle() 실행");
+        // areaCircle()을 오버라이드 했지만 리턴값은 고유값으로 변경함 (리턴값이 달라짐)
         return Math.PI * r * r;
     }
 }
