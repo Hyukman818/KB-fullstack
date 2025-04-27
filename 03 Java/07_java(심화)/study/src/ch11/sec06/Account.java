f044f851f33832792b03bcee7ba975ac2bc6d9a5
Hyukman818 <dhgurtls@gmail.com>
2025-04-27 18:41:34 +0900

    Solving : 07_java(심화) 실습 문제 프로젝트 파일 생성

---
+++ 03 Java/07_java(심화)/study/src/ch11/sec06/Account.java
@@ -0,0 +1,18 @@
+package ch11.sec06;
+
+public class Account {
+    private long balance;
+
+    public Account() { }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void deposit(int money) {
+        balance += money;
+    }
+
+    public void withdraw(int money) {
+    }
+}
---
+++ 03 Java/07_java(심화)/study/src/ch11/sec06/AccountExample.java
@@ -0,0 +1,7 @@
+package ch11.sec06;
+
+public class AccountExample {
+    public static void main(String[] args) {
+
+    }
+}
